e42ea14bdae2c324cd845097b264c153796744e4
anuraglimbu <34967240+anuraglimbu@users.noreply.github.com>
2019-04-28 01:21:21 +0545

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@
 
   $timezone = date_default_timezone_set("Asia/Kathmandu");
 
-  //$con = mysqli_connect("sql302.epizy.com","epiz_23801517","Af5ZI4fej","epiz_23801517_csitascol");
+  
   $con = mysqli_connect("localhost","root","","ascolmanagement");
 
   if(mysqli_connect_errno())
